eafbba458817bd6757aa22879bacb691716d0916
unknown <israeljadesola20000@gmail.com>
2025-08-17 16:54:15 +0100

    Voting Age Checker

---
+++ script.js
@@ -0,0 +1,29 @@
+
+const input = document.getElementById('ageInput');
+const ageResult = document.getElementById('ageText');
+
+function ageBtn() {
+    age = input.value
+    if (age >= 18) {
+        console.log('Citizen can vote');
+        ageResult.textContent = `Hurray! You can vote in the upcoming election`
+    } else if (age == 17) {
+        console.log('Citizen needs 1 year more to vote');
+        ageResult.textContent = `Oops! You need just a year more to be able to vote`
+    } else if (age < 17 && age >= 10) {
+        console.log(`Citizen's age is between 10-16`);
+        ageResult.textContent = `Sorry! You can't vote, you age falls between 10 and 16 `
+    } else if (age < 10 && age >= 5) {
+        console.log(`Citizen's age is between 9-5`);
+        ageResult.textContent = `Sorry! Your age is too low, you age falls between 9 and 5`
+    } else if (age < 5 && age >= 0) {
+        console.log(`Citizen's age is below 5`);
+        ageResult.textContent = `Not really! You can't vote, you age is below 5`
+    } else if (age == 0) {
+        console.log(`Citizen's input 0`);
+        ageResult.textContent = `Error! You can't just be born now (your age can't be 0)`
+    } else {
+        console.log(`Citizen's input negative number`);
+        ageResult.textContent = `Error! You can't input negative number`
+    }
+}
