c9f697606a2b5f1bd8ef6c2149266f80fa8fe313
unknown <israeljadesola20000@gmail.com>
2025-08-19 03:42:41 +0100

    Voters Age calculator

--- script.js
+++ script.js
@@ -12,18 +12,18 @@ function ageBtn() {
         ageResult.textContent = `Oops! You need just a year more to be able to vote`
     } else if (age < 17 && age >= 10) {
         console.log(`Citizen's age is between 10-16`);
-        ageResult.textContent = `Sorry! You can't vote, you age falls between 10 and 16 `
+        ageResult.textContent = `Sorry! You can't vote, your age falls between 10 and 16 `
     } else if (age < 10 && age >= 5) {
         console.log(`Citizen's age is between 9-5`);
-        ageResult.textContent = `Sorry! Your age is too low, you age falls between 9 and 5`
+        ageResult.textContent = `Sorry! Your age is too low, your age falls between 9 and 5`
+    } else if (!age) {
+        console.log(`Citizen did not input age`);
+        ageResult.textContent = `Please input your age`
     } else if (age < 5 && age >= 0) {
-        console.log(`Citizen's age is below 5`);
-        ageResult.textContent = `Not really! You can't vote, you age is below 5`
-    } else if (age == 0) {
         console.log(`Citizen's input 0`);
-        ageResult.textContent = `Error! You can't just be born now (your age can't be 0)`
-    } else {
+        ageResult.textContent = `Ops, You can't vote! (your are not even up to 5)`
+    } else if (age < 0) {
         console.log(`Citizen's input negative number`);
-        ageResult.textContent = `Error! You can't input negative number`
-    }
+        ageResult.textContent = `Error, Age can't be negative! (Pls input a correct age)`
+    } 
 }
